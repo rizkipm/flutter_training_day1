d6483d0f5d8745505dfe7ddfaa9d78554b3f4a11
Rizki Syaputra <udarizki2020@gmail.com>
2026-05-19 14:29:00 +0700

    update

--- lib/pages/page_material_design.dart
+++ lib/pages/page_material_design.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_training_day1/pages/page_flex_list.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -50,8 +51,8 @@ class _PageContentMaterialState extends State<PageContentMaterial> {
   int _selectedIndex = 0;
 
   static const List<Widget> _pages = [
-    PageListVertical(),
-    PageFlexList(),
+    PageSimpleForm(),
+    PageCupertinoIOS(),
     PageButtonMaterial()
   ];
   @override
@@ -101,18 +102,88 @@ class PageButtonMaterial extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Elevated Button'),
+            //Typography
+            Text('Typography', style: Theme.of(context).textTheme.headlineMedium,),
+            SizedBox(height: 10,),
+            Text('Typography', style: Theme.of(context).textTheme.headlineSmall,),
+            SizedBox(height: 10,),
+            Text('Typography', style: Theme.of(context).textTheme.headlineLarge,),
+            SizedBox(height: 10,),
+            Text('Typography', style: Theme.of(context).textTheme.headlineSmall,),
+            SizedBox(height: 10,),
+            Text('Typography', style: Theme.of(context).textTheme.headlineMedium,),
+            SizedBox(height: 10,),
+
+            Card(
+              elevation: 0,
+              color: Theme.of(context).colorScheme.surfaceContainerHighest,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+                side: BorderSide(color: Theme.of(context).colorScheme.outline),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: ElevatedButton(
+                onPressed: () {
+                  //ketike d klik button maka akan muncul dialog
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Dialog Title'),
+                      content: Text('Dialog Content'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Close'),
+                        ),
+                      ],
+                    )
+                  );
+                },
+                child: Text('Elevated Button'),
+              ),
             ),
             SizedBox(height: 10,),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                //Modal bottom sheet
+                showModalBottomSheet(
+                  context: context,
+                  useSafeArea: true,
+                  isScrollControlled: true,
+                  builder: (context) => Container(
+                    padding: EdgeInsets.all(16),
+                    height: 200,
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text('Modal Bottom Sheet'),
+                    ),
+                  ),
+                );
+              },
               child: Text('Outlined Button'),
             ),
             SizedBox(height: 10,),
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                //snackbar
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Item berhasil disimpan'),
+                    duration: Duration(seconds: 5),//5 detik
+                      behavior: SnackBarBehavior.floating,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    margin: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(16),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () {},
+                  )
+                ));
+              },
               child: Text('Text Button'),
             ),
             SizedBox(height: 10,),
@@ -153,6 +224,136 @@ class PageButtonMaterial extends StatelessWidget {
             ),
 
       ]
+        ),
+      ),
+    );
+  }
+}
+
+
+class PageCupertinoIOS extends StatelessWidget {
+  const PageCupertinoIOS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'My IOS APp',
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.activeBlue,
+        scaffoldBackgroundColor: CupertinoColors.white,
+        textTheme: CupertinoTextThemeData(
+          primaryColor: CupertinoColors.black,
+        )
+      ),
+      home: HomeIOSScreen(),
+    );
+  }
+}
+
+class HomeIOSScreen extends StatelessWidget {
+  const HomeIOSScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Cupertino App'),
+            SizedBox(height: 10,),
+            CupertinoButton(
+              onPressed: () async{
+                await Future.delayed(const Duration(seconds: 1));
+                if(!context.mounted) return;
+                showCupertinoDialog(
+                   context: context, builder: (BuildContext context) {
+                     return CupertinoAlertDialog(
+                       title: Text('Cupertino Dialog'),
+                       content: Text('Cupertino Dialog Content'),
+                       actions: [
+                         CupertinoDialogAction(
+                           onPressed: () {
+                             Navigator.pop(context);
+                           },
+                           child: Text('Close'),
+                         ),
+                       ],
+                     );
+                },
+                );
+              },
+              child: Text('Cupertino Button'),
+            )
+          ]
+        )
+      ),
+    );
+  }
+}
+
+
+
+class PageSimpleForm extends StatefulWidget {
+  const PageSimpleForm({super.key});
+
+  @override
+  State<PageSimpleForm> createState() => _PageSimpleFormState();
+}
+
+class _PageSimpleFormState extends State<PageSimpleForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Form'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 10,),
+            Text('Simple Form'),
+            SizedBox(height: 10,),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Masukkan Username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  ),
+                keyboardType: TextInputType.name,//hanya huruf aja
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z]')),
+                ],
+                ),
+            ),
+
+            Padding(padding: EdgeInsetsGeometry.all(10),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,//hanya angka aja
+                  LengthLimitingTextInputFormatter(10),
+                ],
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  hintText: 'Masukkan Nomor Telepon',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+
+              ),
+            )),
+          ],
         ),
       ),
     );
